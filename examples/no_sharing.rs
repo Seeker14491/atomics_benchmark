a4934a78acdfe630ea00f822a9cbc5bbba1347be
Brian Bowman <seeker14491@gmail.com>
2016-03-18 01:10:42 -0500

    make benchmark fairer

--- examples/no_sharing.rs
+++ examples/no_sharing.rs
@@ -15,13 +15,18 @@ fn main() {
     let rand_range = Range::new(0, atomics_benchmark::ARRAY_SIZE);
     let num_threads = num_cpus::get();
 
+    let mut arrays = Vec::with_capacity(num_threads);
+    for _ in 0..num_threads {
+        arrays.push(vec![0; atomics_benchmark::ARRAY_SIZE].into_boxed_slice());
+    }
+
     let mut thread_handles = Vec::with_capacity(num_threads);
 
     println!("{} seconds", timeit_loops!(1, {
         for _ in 0..num_threads {
+            let mut array = arrays.pop().unwrap();
             thread_handles.push(thread::spawn(move || {
                 let mut rng = rand::weak_rng();
-                let mut array = vec![0; atomics_benchmark::ARRAY_SIZE].into_boxed_slice();
                 for _ in 0..atomics_benchmark::ITERATIONS_PER_THREAD {
                     let random_index = rand_range.ind_sample(&mut rng);
                     array[random_index] += 1;
